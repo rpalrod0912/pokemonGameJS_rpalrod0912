847989e8fa428450add8d792f5b171112923192a
rpalrod0912 <rpalrod0912@g.educaand.es>
2023-02-03 19:12:25 +0100

    created components

---
+++ pokemonGameRpalrod/src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+
+import "./assets/main.css";
+import "./assets/styles.css";
+import "./assets/animations.css";
+
+createApp(App).mount("#app");
---
+++ pokemonGameRpalrod/src/pages/PokemonPage.vue
@@ -0,0 +1,13 @@
+import PokemonPictures from '../components/PokemonPictures.vue';
+
+<template>
+  <div>
+    <h1>¿Cuál es este Pokémon?</h1>
+
+    <PokemonPictures></PokemonPictures>
+  </div>
+</template>
+
+<script></script>
+
+<style></style>
