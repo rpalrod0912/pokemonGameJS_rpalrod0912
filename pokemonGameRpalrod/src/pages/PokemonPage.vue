cb8c93aa03d89b63389908fc1b192eb1a4d05f4d
rpalrod0912 <rpalrod0912@g.educaand.es>
2023-02-03 22:08:11 +0100

    Si acierta muestra descripcion pokemon

--- pokemonGameRpalrod/src/pages/PokemonPage.vue
+++ pokemonGameRpalrod/src/pages/PokemonPage.vue
@@ -1,14 +1,15 @@
 <template>
   <h1 v-if="!pokemon">Cargando...</h1>
   <div v-else="pokemon">
-    <h1>Tu puntuacion:{{ this.puntuacion }}</h1>
     <h1>¿Cuál es este Pokémon?</h1>
 
     <PokemonPicture
       :pokemonId="pokemon.id"
       :mostrarPkm="showPkm"
     ></PokemonPicture>
-
+    <h2 v-if="tipo">
+      {{ this.descripcionPokemon }}
+    </h2>
     <PokemonOptions
       :pokemons="pkmnsArr"
       @selection="verificarRes"
@@ -16,12 +17,16 @@
 
     <template v-if="mostrarResp">
       <h2 class="fade-in">{{ msj }}</h2>
-      <button @click="nuevoJuego(this.tipo)">Empezar de Nuevo</button>
+      <button @click="nuevoJuego(this.tipo)">
+        {{ tipo ? "Siguiente Pokemon" : "Nueva Partida" }}
+      </button>
     </template>
+    <h1>Tu puntuacion:{{ this.puntuacion }}</h1>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 import PokemonOptions from "../components/PokemonOptions.vue";
 import PokemonPicture from "../components/PokemonPicture.vue";
 import getPokemonOptions from "../helpers/getPokemonOptions";
@@ -37,15 +42,38 @@ export default {
       msj: "",
       puntuacion: 0,
       comenzarDeNuevo: false,
+      //Booleano tipo para verificar si el jugador a acertado o ha fallado
       tipo: false,
+      descripcionPokemon: "",
     };
   },
   methods: {
+    async getPokemonDescription(id) {
+      let url = `https://pokeapi.co/api/v2/pokemon-species/${id}/`;
+      const data = axios.get(url);
+      const datosPkmn = await data;
+      debugger;
+      //Filtramos texto en español de la API
+      const filteredFlavorTextEntries =
+        datosPkmn.data.flavor_text_entries.filter(
+          (element) => element.language.name === "es"
+        );
+      const flavorTextEntry =
+        filteredFlavorTextEntries.length > 0
+          ? filteredFlavorTextEntries[0]
+          : {};
+      const flavorText = flavorTextEntry.flavor_text;
+      this.descripcionPokemon = flavorText;
+    },
+
     async mixedPkmArr() {
+      debugger;
       this.pkmnsArr = await getPokemonOptions();
       const intRand = Math.floor(Math.random() * 4);
       this.pokemon = this.pkmnsArr[intRand];
       console.log(this.pokemon.id);
+      debugger;
+      this.getPokemonDescription(this.pokemon.id);
     },
     verificarRes(selectId) {
       this.showPkm = true;
@@ -60,6 +88,7 @@ export default {
         this.tipo = false;
       }
     },
+    //Añadido Sistema de puntuacion si variable tipo false reseteamos contador
     nuevoJuego(tipo) {
       if (!tipo) {
         this.puntuacion = 0;
@@ -68,11 +97,14 @@ export default {
         (this.mostrarResp = false),
         (this.pkmnsArr = []),
         this.mixedPkmArr(),
-        (this.pokemon = null);
+        (this.pokemon = null),
+        (this.tipo = false),
+        (this.descripcionPokemon = "");
     },
   },
   mounted() {
     this.mixedPkmArr();
+    debugger;
   },
 };
 </script>
