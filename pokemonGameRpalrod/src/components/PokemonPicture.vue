d751df5a77e57bd8ad29ab473c5a0cdd60bc1d34
rpalrod0912 <rpalrod0912@g.educaand.es>
2023-02-03 19:25:15 +0100

    css Aplicado

---
+++ pokemonGameRpalrod/src/components/PokemonPicture.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="pokemon-container">
+    <img
+      src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+      alt="pokemonImage"
+      class="hidden-pokemon"
+    />
+    <!--
+        PARA MAS TARDE....
+        <img
+      src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+      alt="pokemon"
+      class="fade-in" /> -->
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.pokemon-container {
+  height: 200px;
+  position: relative;
+}
+
+img {
+  height: 200px;
+  position: absolute;
+  left: 50%;
+  user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  -webkit-user-drag: none;
+  -webkit-user-select: none;
+
+  transform: translateX(-50%);
+}
+.hidden-pokemon {
+  filter: brightness(0);
+}
+</style>
--- pokemonGameRpalrod/src/pages/PokemonPage.vue
+++ pokemonGameRpalrod/src/pages/PokemonPage.vue
@@ -4,10 +4,19 @@ import PokemonPictures from '../components/PokemonPictures.vue';
   <div>
     <h1>¿Cuál es este Pokémon?</h1>
 
-    <PokemonPictures></PokemonPictures>
+    <PokemonPicture></PokemonPicture>
+
+    <PokemonOptions></PokemonOptions>
   </div>
 </template>
 
-<script></script>
+<script>
+import PokemonOptions from "../components/PokemonOptions.vue";
+import PokemonPicture from "../components/PokemonPicture.vue";
+
+export default {
+  components: { PokemonOptions, PokemonPicture },
+};
+</script>
 
 <style></style>
